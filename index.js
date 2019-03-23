b73a4de71e2feb172eb1c792b515301d4c910a82
Subhajit Sahu <wolfram77@gmail.com>
2019-03-23 20:10:42 +0530

    fix: unexpected token h; needed to pad zeros in lenStr

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function messageParse(msg) {
 
 function messageStringify(head, body) {
   var headStr = JSON.stringify(head);
-  var lenStr = headStr.length.toString(16);
+  var lenStr = headStr.length.toString(16).padStart(8, '0');
   return lenStr+headStr+body;
 };
 
@@ -21,3 +21,10 @@ function Link(address, options) {
   var o = options||{};
   var socket = new WebSocket(address, options);
 };
+
+
+var msg1 = messageStringify({type: 'http.post', source: '127.0.0.1:64123', target: '?'}, 'Hey!');
+console.log('msg1:', msg1);
+var {head, body} = messageParse(msg1);
+console.log('head:', head);
+console.log('body:', body);
