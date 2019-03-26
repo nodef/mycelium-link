3c92b758594c621bd811f374c77d2627b8fae48b
Subhajit Sahu <wolfram77@gmail.com>
2019-03-26 09:21:30 +0530

    update packet structure

---
+++ index.js
@@ -0,0 +1,2 @@
+if(typeof WebSocket==='undefined') WebSocket = require('ws');
+console.log(WebSocket, WebSocket.Server);
--- packet.js
+++ packet.js
@@ -1,14 +1,17 @@
 function parse(msg) {
-  var len = parseInt(msg.substring(0, 8), 16);
-  var head = JSON.parse(msg.substring(8, 8+len));
-  var body = msg.substring(8+len);
-  return {head, body};
+  var type = msg.substring(0, 4).trimEnd();
+  var size = parseInt(msg.substring(4, 8), 16);
+  var head = JSON.parse(msg.substring(8, 8+size));
+  var body = msg.substring(8+size);
+  return {type, head, body};
 };
 
-function stringify(head, body) {
+function stringify(type, head, body) {
+  var bodyStr = body||'';
   var headStr = JSON.stringify(head);
-  var lenStr = headStr.length.toString(16).padStart(8, '0');
-  return lenStr+headStr+(body||'');
+  var sizeStr = headStr.length.toString(16).padStart(4, '0');
+  var typeStr = type.padEnd(4);
+  return typeStr+sizeStr+headStr+bodyStr;
 };
 
 
