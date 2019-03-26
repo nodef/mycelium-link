be9a0a10f6cdfe075d42140bafa563f2105de8d6
Subhajit Sahu <wolfram77@gmail.com>
2019-03-26 09:23:40 +0530

    update packet structure

--- packet.js
+++ packet.js
@@ -1,17 +1,17 @@
 function parse(msg) {
-  var type = msg.substring(0, 4).trimEnd();
+  var protocol = msg.substring(0, 4).trimEnd();
   var size = parseInt(msg.substring(4, 8), 16);
   var head = JSON.parse(msg.substring(8, 8+size));
   var body = msg.substring(8+size);
-  return {type, head, body};
+  return {protocol, head, body};
 };
 
-function stringify(type, head, body) {
+function stringify(protocol, head, body) {
   var bodyStr = body||'';
   var headStr = JSON.stringify(head);
   var sizeStr = headStr.length.toString(16).padStart(4, '0');
-  var typeStr = type.padEnd(4);
-  return typeStr+sizeStr+headStr+bodyStr;
+  var protocolStr = protocol.padEnd(4);
+  return protocolStr+sizeStr+headStr+bodyStr;
 };
 
 
