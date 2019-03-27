da5eb01df952b3163e4c5738ba36212b39e9395f
Subhajit Sahu <wolfram77@gmail.com>
2019-03-27 13:42:04 +0530

    see: net.createSErver

--- net.js
+++ net.js
@@ -4,6 +4,8 @@ const EventEmitter = require('events');
 
 function Server(options, connectionListener) {
   EventEmitter.call(this);
+  // SEE: net.createServer()
+  if(connectionListener) this.on('connection', connectionListener);
 };
 Server.prototype = new EventEmitter();
 
